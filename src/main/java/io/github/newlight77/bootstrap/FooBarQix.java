503b444762de962142ddc66bfc1159ee6746f0c8
Salah Eddine <salaheddine@macbook-pro-de-salah.home>
2022-04-01 21:09:58 +0200

    handle corner cases

--- src/main/java/io/github/newlight77/bootstrap/FooBarQix.java
+++ src/main/java/io/github/newlight77/bootstrap/FooBarQix.java
@@ -1,7 +1,61 @@
 package io.github.newlight77.bootstrap;
 
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.function.BiPredicate;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
 public class FooBarQix {
-    public String compute(String s) {
-        return s;
+
+    public static final String FOO = "Foo";
+    public static final String BAR = "Bar";
+    public static final String QIX = "Qix";
+
+
+    public String compute(String input) {
+        Integer inputAsInt = Integer.parseInt(input);
+
+        StringBuilder result = new StringBuilder();
+        appendIfDivision(inputAsInt, result);
+
+        char[]  chars = input.toCharArray();
+
+        for (int i = 0; i < chars.length; i++) {
+            if(chars[i] == '3'){
+                result.append(FOO);
+            }
+        }
+        if(result.isEmpty()){
+            result.append(input);
+        }
+        return result.toString();
+    }
+
+    private void appendIfDivision(Integer inputAsInt, StringBuilder result) {
+
+        Arrays.stream(Division.values()).forEach( d -> {
+            if(DivisionPredicate.denominatorPredicate.test(inputAsInt , d.denominator)){
+                result.append(d.name());
+            }
+        });
+
+    }
+
+    class DivisionPredicate {
+        private static BiPredicate<Integer, String> denominatorPredicate =  (req, d) -> req % Integer.parseInt(d) == 0;
+    }
+
+    enum Division {
+        Foo("3"),
+        Bar( "5"),
+        Qix("7");
+
+        private Division(String denominator){
+            this.denominator = denominator;
+        }
+
+        private String denominator;
+
     }
 }
--- src/test/java/io/github/newlight77/bootstrap/FooBarQixTest.java
+++ src/test/java/io/github/newlight77/bootstrap/FooBarQixTest.java
@@ -2,14 +2,21 @@ package io.github.newlight77.bootstrap;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class FooBarQixTest {
 
+    private FooBarQix fooBarQix;
+
+    @BeforeEach
+    void setUp(){
+        fooBarQix = new FooBarQix();
+    }
+
     @Test
     void shouldReturnAsIs_WhenIsOne() {
         // Given
-        FooBarQix fooBarQix = new FooBarQix();
 
         // When
         String result = fooBarQix.compute("1");
@@ -21,7 +28,6 @@ public class FooBarQixTest {
     @Test
     void shouldReturnAsIs_WhenIsTwo() {
         // Given
-        FooBarQix fooBarQix = new FooBarQix();
 
         // When
         String result = fooBarQix.compute("2");
@@ -30,5 +36,47 @@ public class FooBarQixTest {
         assertEquals( "2", result);
     }
 
+    @Test
+    void shouldReturnFooFoo_WhenTheNumberIs3() {
+        // Given
 
+        // When
+        String result = fooBarQix.compute("3");
+
+        // Then
+        assertEquals( "FooFoo", result);
+    }
+
+    @Test
+    void shouldReturnFoo_WhenTheNumberIs6() {
+        // Given
+
+        // When
+        String result = fooBarQix.compute("6");
+
+        // Then
+        assertEquals( "Foo", result);
+    }
+
+    @Test
+    void shouldReturnBar_WhenTheNumberIs5() {
+        // Given
+
+        // When
+        String result = fooBarQix.compute("5");
+
+        // Then
+        assertEquals( "Bar", result);
+    }
+
+    @Test
+    void shouldReturnQix_WhenTheNumberIs7() {
+        // Given
+
+        // When
+        String result = fooBarQix.compute("7");
+
+        // Then
+        assertEquals( "Qix", result);
+    }
 }
